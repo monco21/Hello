b7a1ac085f5b501788eaf0b3a85e874e18efae02
monco21 <simeonov21@gmail.com>
2016-10-29 20:49:45 +0100

    last

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
             <body>
                 <p>
                     <?php
-                    echo "Hello,"."  wqwasqdaeqwe  ". "world". "!"
+                    echo "Hello,"." tuto finito chikita   ". "world". "!"
 
                     ?>
                 </p>
