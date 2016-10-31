ac2c5a98fa0e2380487602a1daaf017432069d05
monco21 <simeonov21@gmail.com>
2016-10-31 15:52:39 +0000

    last

--- index.php
+++ index.php
@@ -16,30 +16,10 @@
                             print "<p>$x</p>";
 
                             for($i = 1; $i < 31; $i++){
-                                If(($i/2 )&& ($i/3) && ($i/4)){
+                                If($i/2 && $i/3 && $i/4){
                                     echo "On the $i of the month Mugs, Sausage Rolls and Specs are available";
                                 }
-                                elseif(($i/2) && ($i/3)){
-                                    echo "On the $i of the month Mugs and Specs are available";
-                                }
-                                elseif(($i/2) && ($i/4)){
-                                    echo "On the $i of the month Mugs and Sausage Rolls are available";
-                                }
-                                elseif(($i/3) && ($i/4)){
-                                    echo "On the $i of the month Specs and Sausage Rolls are available";
-                                }
-                                elseif($i/2){
-                                    echo "On the $i of the month Specs are available";
-                                }
-                                elseif ($i/3){
-                                    echo "On the $i of the month Mugs are available";
-                                }
-                                elseif($i/4){
-                                    echo "On the $i of the month Sausage Rolls are available";
-                                }
-                                else{
-                                    echo "On the $i of the month no products are available";
-                                }
+
 
                             }
 
