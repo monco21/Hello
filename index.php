318d4ddfc7c6d6876ee4c0a3a1b77c19541fc636
monco21 <simeonov21@gmail.com>
2016-11-01 09:33:41 +0000

    last

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
                     <?php
                         $provisionedActivities = array("Specs", "Drugs", "Rock and Roll");
 
-                        foreach($provisionedActivities as $x){
+                        foreach($provisionedActivities as $x) {
                             print "<p>$x</p>";
 
                             $provisionedActivities[1] = "hugs";
                             print "<p>$x</p>";
                             $provisionedActivities[2] = "";
                             print "<p>$x</p>";
-
+                        }
                             for($i = 1; $i < 31; $i++){
                                 If($i%2==0 && $i%3==0 && $i%4==0){
                                     echo "<br>On the $i of the month Mugs, Specs and Sausage are available";
@@ -46,7 +46,7 @@
 
                             }
 
-                        }
+
                     ?>
                 </p>
             </body>
