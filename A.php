65e8504261f7d91bfac5c84c41a0d59aece2c46d
monco21 <simeonov21@gmail.com>
2016-11-03 17:04:01 +0000

    last

--- A.php
+++ A.php
@@ -21,14 +21,11 @@ $result1 = $db->query($sql_query);
 $result2 = $db->query($sql_query2);
 $result3 = $db->query($sql_query3);
 
-while($row = $result1->fetch_array()){
-    while($row = $result->fetch_array()) {
+
         while($row = $result2->fetch_array()) {
             echo " $row[title]----$row[productionStudio]----$row[yearReleased]";
             echo "<br>";
         }
-    }
-}
 
 while($row = $result3->fetch_array()){
     echo "$row[title]";
