69ce2c5726d0fdd9548733fe433e7d630818091d
monco21 <simeonov21@gmail.com>
2016-11-03 11:24:21 +0000

    last

--- A.php
+++ A.php
@@ -15,17 +15,24 @@ include("dbconnect.php");
 $sql_query = "SELECT*FROM marvelmovies";
 $sql_query1 = "SELECT*FROM marvelmovies WHERE productionStudio LIKE '%Marvel Studio%'";
 $sql_query2 = "Select*FROM marvelmovies WHERE yearReleased > '2010' AND productionStudio LIKE '%Marvel Studio%'";
+$sql_query3 = "SELECT*FROM marvelmovies WHERE title LIKE '%X-Men%'";
 $result = $db->query($sql_query1);
 $result1 = $db->query($sql_query);
 $result2 = $db->query($sql_query2);
+$result3 = $db->query($sql_query3);
 
 while($row = $result1->fetch_array()){
     while($row = $result->fetch_array()) {
         while($row = $result2->fetch_array()) {
             echo " $row[title]----$row[productionStudio]----$row[yearReleased]";
-            echo "<br>,";
+            echo "<br>";
         }
     }
+}
+
+while($row = $result3->fetch_array()){
+    echo "$row[title]";
+    echo "<br>";
 }
 
 
