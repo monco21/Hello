8537d9df4debf7f844850ef7e02538fc8c7139c9
monco21 <simeonov21@gmail.com>
2016-11-03 15:05:06 +0000

    last

--- submitform.php
+++ submitform.php
@@ -5,3 +5,21 @@
  * Date: 3.11.2016 г.
  * Time: 13:57
  */
+
+include ("db_connect.php");
+
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$gender = $_POST["gender"];
+$power = $_POST["power"];
+
+$sql = "INSERT INTO newusers (firstname,lastname,gender,power) VALUES ('$firstname','$lastname','$gender','$power')";
+
+if(mysqli_query($db,$sql)){
+    else{
+        echo "Error: ".sql."<br>".mysqli_error($db);
+    }
+    header("location:viewusers.php");
+
+}
+?>
