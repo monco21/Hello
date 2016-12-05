3d1176eaa2992d509ab63ceb40b568db661f76fa
monco21 <simeonov21@gmail.com>
2016-12-05 18:02:06 +0000

    last

--- userView.php
+++ userView.php
@@ -11,6 +11,7 @@
 <p>The following users have registered on the site:</p>
 <ul>
     <?
+    $sql=mysqli_query("SELECT FROM users (username, password, email) WHERE username=$username");
     $sql_query="SELECT*FROM users";
     $result = $db->query($sql_query);
     while($row = $result->fetch_array()){
@@ -19,7 +20,7 @@
         $email = $_POST["email"];
         $phoneNumber = $_POST["phoneNumber"];
         $typeOfUser = $_POST["typeOfUser"];
-        echo "<li>{$username}   {$password}  {$email}   {$phoneNumber} {$typeOfUser}</li>>";
+        echo "<li>{$sql} {$username}   {$password}  {$email}   {$phoneNumber} {$typeOfUser}</li>>";
     }
     ?>
 </ul>
