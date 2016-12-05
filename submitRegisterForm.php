48d543d792099229bf88f6c6fb663089aeec8df8
monco21 <simeonov21@gmail.com>
2016-12-05 15:02:17 +0000

    last

--- submitRegisterForm.php
+++ submitRegisterForm.php
@@ -14,22 +14,19 @@ $email = $_POST["email"];
 $phoneNumber = $_POST["phoneNumber"];
 $typeOfUser = $_POST["typeOfUser"];
 
-$sql = "SELECT * FROM tbl_Freelancers WHERE username = '".$username."'";
-$result = mysqli_query($con,$query);
-
-if(mysqli_num_rows($result)>=1)//You are mixing the mysql and mysqli, change this line of code
-{
-    echo"name already exists";
-}
-else {
-
+$sql = "SELECT * FROM users WHERE username = '".$username."'";
+$result = mysqli_query($sql);
+if(mysqli_num_rows($result)!=0){
+    echo "name already exist";
+}else{
     $sql = "INSERT INTO users(username,password,email,phoneNumber,typeOfUser) VALUES ('$username','$password','$email','$phoneNumber','$typeOfUser')";
 
-    if (mysqli_query($db, $sql)) {
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($db);
+    if(mysqli_query($db,$sql)){
+    } else{
+        echo "Error: ".$sql."<br>".mysqli_error($db);
     }
 }
+
 header("location:userView.php");
 
 
