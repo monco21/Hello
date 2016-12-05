1312960880a268821e25d271ffa4d99ba7b63c8d
monco21 <simeonov21@gmail.com>
2016-12-05 15:51:21 +0000

    last

--- submitRegisterForm.php
+++ submitRegisterForm.php
@@ -15,26 +15,27 @@ $phoneNumber = $_POST["phoneNumber"];
 $typeOfUser = $_POST["typeOfUser"];
 
 
-    $sql = 'SELECT * FROM users WHERE username = $username';
+$sql = 'SELECT * FROM users WHERE username = $username';
 $result=mysqli_query($sql);
 
-if(mysqli_num_rows($result)!=0)
+if(mysqli_num_rows($result)>0)
 {
     echo"name already exists";
 }
 else
 {
     $sql = "INSERT INTO users(username,password,email,phoneNumber,typeOfUser) VALUES ('$username','$password','$email','$phoneNumber','$typeOfUser')";
-}
+    if(mysqli_query($db,$sql)){
+    } else{
+        echo "Error: ".$sql."<br>".mysqli_error($db);
+    }
 
-
-
-
-if(mysqli_query($db,$sql)){
-} else{
-    echo "Error: ".$sql."<br>".mysqli_error($db);
 }
 header("location:userView.php");
+
+
+
+
 
 
 
