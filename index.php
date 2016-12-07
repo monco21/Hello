d539e88487e1c449a654b92e88b9b1ed6aa2a220
monco21 <simeonov21@gmail.com>
2016-12-07 12:27:24 +0000

    last

--- index.php
+++ index.php
@@ -1,57 +1,22 @@
-<!DOCTYPE html>
-    <html>
-        <head>
-            </head>
-            <body>
-                <p>
-                    <?php
-                            $mugsCount = 7;
-                            $specsCount = 7;
-                            $sausageCount = 7;
-                           for ($i = 1; $i < 31; $i++) {
-                               $thisMonthGoods = rand(0,2);
+<form method="post">
+    <table>
+    <tr>
+        <td>Username:</td>
+        <td><input type="text" name="username"/></td>
+    </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="password" name="password"/></td>
+        </tr>
+        <tr>
+            <td>Repeat Password:</td>
+            <td><input type="passsword" name="rpassword"/></td>
+        </tr>
+        <tr>
+            <td>Email:</td>
+            <td><input type="email" name="email"/></td>
+        </tr>
+    </table>
 
-                            if($thisMonthGoods == 0 && $mugsCount > 0){
-                                    echo "<br> On the $i of the month Mugs are x";
-                                $mugsCount--;
-                            }
-                            elseif ($thisMonthGoods == 1 && $sausageCount > 0){
-                                    echo "<br> On the $i of the month Specs are available";
-                                $sausageCount--;
-                            }
-                            elseif ($thisMonthGoods == 2 && $sausageCount > 0){
-                                    echo "<br>On the $i of the month Sausage are available";
-                                $sausageCount--;
-                            }
-                            else{
-                                echo "<br>No more goods are available this month";
-                            }
-
-
-
-
-
-                              /* If ($i % 2 == 0 && $i % 3 == 0 && $i % 4 == 0) {
-                                   echo "<br>On the $i of the month Mugs, Specs and Sausage are available";
-                               } elseif ($i % 2 == 0 && $i % 3 == 0) {
-                                   echo "<br>On the $i of the month Mugs and Specs are available";
-                               } elseif ($i % 2 == 0 && $i % 4) {
-                                   echo "<br> On the $i of the month Mugs and Sausage are available";
-                               } elseif ($i % 3 == 0 && $i % 4 == 0) {
-                                   echo "<br> On the $i of the month Specs and Sausage are available";
-                               } elseif ($i % 2 == 0) {
-                                   echo "<br> On the $i of the month Mugs are available";
-                               } elseif ($i % 3 == 0) {
-                                   echo "<br> On the $i of the month Specs are available";
-                               } elseif ($i % 4 == 0) {
-                                   echo "<br> On the $i of the month Sausages are available";
-                               } else {
-                                   echo "<br>On the $i of the month no products are available";
-                               }*/
-                           }
-
-
-                    ?>
-                </p>
-            </body>
-        </html>
+    <input type="submit" name="submit" value="Register"/>
+</form>
